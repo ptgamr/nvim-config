b97b67e2e7f7626ea18689148d481d3717115fee
Anh Trinh <anh.trinhtrung@gmail.com>
2023-05-16 01:04:25 +1200

    Enable kickstarter autoformat and avoid conflicting

--- lua/custom/plugins/null-ls.lua
+++ lua/custom/plugins/null-ls.lua
@@ -1,15 +1,12 @@
--- NOT WORKING for monorepo
--- The cwd is somehow resolved to the child project of monorepo (because it contains package.json)
--- and it's not possible to override it as stated in the doc
+-- VERY IMPORTANT TO NOTE
+-- prettier formatter is conflicting  with tsserver default lsp formatting
+-- we have to disable it, @see autocomplete.lua
+-- Reference: https://github.com/jose-elias-alvarez/null-ls.nvim/wiki/Avoiding-LSP-formatting-conflicts
 return {
   "jose-elias-alvarez/null-ls.nvim",
   version = "*",
   config = function ()
     local null_ls = require("null-ls")
-    local group = vim.api.nvim_create_augroup("lsp_format_on_save", { clear = false })
-    local event = "BufWritePre" -- or "BufWritePost"
-    local async = event == "BufWritePost"
-
     null_ls.setup({
       debug = true,
       sources = {
@@ -25,32 +22,7 @@ return {
                 return vim.loop.cwd()
             end,
           })
-          -- null_ls.builtins.formatting.stylua,
       },
-      on_attach = function(client, bufnr)
-        if client.supports_method("textDocument/formatting") then
-          vim.keymap.set("n", "<Leader>f", function()
-            vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf() })
-          end, { buffer = bufnr, desc = "[lsp] format" })
-
-          -- format on save
-          vim.api.nvim_clear_autocmds({ buffer = bufnr, group = group })
-          vim.api.nvim_create_autocmd(event, {
-            buffer = bufnr,
-            group = group,
-            callback = function()
-              vim.lsp.buf.format({ bufnr = bufnr, async = async })
-            end,
-            desc = "[lsp] format on save",
-          })
-        end
-
-        if client.supports_method("textDocument/rangeFormatting") then
-          vim.keymap.set("x", "<Leader>f", function()
-            vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf() })
-          end, { buffer = bufnr, desc = "[lsp] format" })
-        end
-      end,
     })
   end
 }
