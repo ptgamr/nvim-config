6148531ee42cd60585b5546cc7314c5aa09a1944
Anh <anh.trinhtrung@gmail.com>
2023-05-20 02:15:04 +1200

    disable Neotree fuzzy default "/" mapping to fuzzy filter

--- lua/custom/plugins/filetree.lua
+++ lua/custom/plugins/filetree.lua
@@ -12,19 +12,19 @@ return {
   -- config = function ()
   --   require('neo-tree').setup {}
   -- end,
-  config = function ()
+  config = function()
     -- Unless you are still migrating, remove the deprecated commands from v1.x
     vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
 
     -- If you want icons for diagnostic errors, you'll need to define them somewhere:
     vim.fn.sign_define("DiagnosticSignError",
-      {text = " ", texthl = "DiagnosticSignError"})
+      { text = " ", texthl = "DiagnosticSignError" })
     vim.fn.sign_define("DiagnosticSignWarn",
-      {text = " ", texthl = "DiagnosticSignWarn"})
+      { text = " ", texthl = "DiagnosticSignWarn" })
     vim.fn.sign_define("DiagnosticSignInfo",
-      {text = " ", texthl = "DiagnosticSignInfo"})
+      { text = " ", texthl = "DiagnosticSignInfo" })
     vim.fn.sign_define("DiagnosticSignHint",
-      {text = "", texthl = "DiagnosticSignHint"})
+      { text = "", texthl = "DiagnosticSignHint" })
     -- NOTE: this is changed from v1.x, which used the old style of highlight groups
     -- in the form "LspDiagnosticsSignWarning"
 
@@ -34,8 +34,8 @@ return {
       enable_git_status = true,
       enable_diagnostics = true,
       open_files_do_not_replace_types = { "terminal", "trouble", "qf" }, -- when opening files, do not use windows containing these filetypes or buftypes
-      sort_case_insensitive = false, -- used when sorting files and directories in the tree
-      sort_function = nil , -- use a custom function for sorting files and directories in the tree 
+      sort_case_insensitive = false,                                     -- used when sorting files and directories in the tree
+      sort_function = nil,                                               -- use a custom function for sorting files and directories in the tree
       -- sort_function = function (a,b)
       --       if a.type == b.type then
       --           return a.path > b.path
@@ -82,10 +82,10 @@ return {
         git_status = {
           symbols = {
             -- Change type
-            added     = "", -- or "✚", but this is redundant info if you use git_status_colors on the name
-            modified  = "", -- or "", but this is redundant info if you use git_status_colors on the name
-            deleted   = "✖",-- this can only be used in the git_status source
-            renamed   = "",-- this can only be used in the git_status source
+            added     = "",  -- or "✚", but this is redundant info if you use git_status_colors on the name
+            modified  = "",  -- or "", but this is redundant info if you use git_status_colors on the name
+            deleted   = "✖", -- this can only be used in the git_status source
+            renamed   = "", -- this can only be used in the git_status source
             -- Status type
             untracked = "",
             ignored   = "",
@@ -107,9 +107,9 @@ return {
           nowait = true,
         },
         mappings = {
-          ["<space>"] = { 
-              "toggle_node", 
-              nowait = false, -- disable `nowait` if you have existing combos starting with this char that you want to use 
+          ["<space>"] = {
+            "toggle_node",
+            nowait = false, -- disable `nowait` if you have existing combos starting with this char that you want to use
           },
           ["<2-LeftMouse>"] = "open",
           ["<cr>"] = "open",
@@ -129,7 +129,7 @@ return {
           -- ['C'] = 'close_all_subnodes',
           ["z"] = "close_all_nodes",
           --["Z"] = "expand_all_nodes",
-          ["a"] = { 
+          ["a"] = {
             "add",
             -- this command supports BASH style brace expansion ("x{a,b,c}" -> xa,xb,xc). see `:h neo-tree-file-actions` for details
             -- some commands may take optional config options, see `:h neo-tree-mappings` for details
@@ -156,6 +156,8 @@ return {
           ["?"] = "show_help",
           ["<"] = "prev_source",
           [">"] = "next_source",
+          -- disable fuzzy finder, restore normal buffer search
+          ["/"] = "noop"
         }
       },
       nesting_rules = {},
@@ -183,22 +185,22 @@ return {
             --".null-ls_*",
           },
         },
-        follow_current_file = false, -- This will find and focus the file in the active buffer every
-                                     -- time the current file is changed while the tree is open.
-        group_empty_dirs = false, -- when true, empty folders will be grouped together
+        follow_current_file = false,            -- This will find and focus the file in the active buffer every
+        -- time the current file is changed while the tree is open.
+        group_empty_dirs = false,               -- when true, empty folders will be grouped together
         hijack_netrw_behavior = "open_default", -- netrw disabled, opening a directory opens neo-tree
-                                                -- in whatever position is specified in window.position
-                              -- "open_current",  -- netrw disabled, opening a directory opens within the
-                                                -- window like netrw would, regardless of window.position
-                              -- "disabled",    -- netrw left alone, neo-tree does not handle opening dirs
+        -- in whatever position is specified in window.position
+        -- "open_current",  -- netrw disabled, opening a directory opens within the
+        -- window like netrw would, regardless of window.position
+        -- "disabled",    -- netrw left alone, neo-tree does not handle opening dirs
         use_libuv_file_watcher = false, -- This will use the OS level file watchers to detect changes
-                                        -- instead of relying on nvim autocmd events.
+        -- instead of relying on nvim autocmd events.
         window = {
           mappings = {
             ["<bs>"] = "navigate_up",
             ["."] = "set_root",
             ["H"] = "toggle_hidden",
-            ["/"] = "fuzzy_finder",
+            ["<C-s>"] = "fuzzy_finder",
             ["D"] = "fuzzy_finder_directory",
             ["#"] = "fuzzy_sorter", -- fuzzy sorting using the fzy algorithm
             -- ["D"] = "fuzzy_sorter_directory",
@@ -207,7 +209,8 @@ return {
             ["[g"] = "prev_git_modified",
             ["]g"] = "next_git_modified",
           },
-          fuzzy_finder_mappings = { -- define keymaps for filter popup window in fuzzy_finder_mode
+          fuzzy_finder_mappings = {
+            -- define keymaps for filter popup window in fuzzy_finder_mode
             ["<down>"] = "move_cursor_down",
             ["<C-n>"] = "move_cursor_down",
             ["<up>"] = "move_cursor_up",
@@ -219,8 +222,8 @@ return {
       },
       buffers = {
         follow_current_file = false, -- This will find and focus the file in the active buffer every
-                                     -- time the current file is changed while the tree is open.
-        group_empty_dirs = true, -- when true, empty folders will be grouped together
+        -- time the current file is changed while the tree is open.
+        group_empty_dirs = true,     -- when true, empty folders will be grouped together
         show_unloaded = true,
         window = {
           mappings = {
