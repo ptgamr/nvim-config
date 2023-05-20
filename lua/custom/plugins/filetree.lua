9d7ec308f48f01304f5b8e787bf5e11dfa278916
Anh Trinh <anh.trinhtrung@gmail.com>
2023-05-20 15:05:41 +1200

    Rid of those icons

--- lua/custom/plugins/filetree.lua
+++ lua/custom/plugins/filetree.lua
@@ -6,25 +6,22 @@ return {
   version = "*",
   dependencies = {
     "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+    -- "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
     "MunifTanjim/nui.nvim",
   },
-  -- config = function ()
-  --   require('neo-tree').setup {}
-  -- end,
   config = function()
     -- Unless you are still migrating, remove the deprecated commands from v1.x
     vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
 
     -- If you want icons for diagnostic errors, you'll need to define them somewhere:
-    vim.fn.sign_define("DiagnosticSignError",
-      { text = " ", texthl = "DiagnosticSignError" })
-    vim.fn.sign_define("DiagnosticSignWarn",
-      { text = " ", texthl = "DiagnosticSignWarn" })
-    vim.fn.sign_define("DiagnosticSignInfo",
-      { text = " ", texthl = "DiagnosticSignInfo" })
-    vim.fn.sign_define("DiagnosticSignHint",
-      { text = "", texthl = "DiagnosticSignHint" })
+    -- vim.fn.sign_define("DiagnosticSignError",
+    --   { text = " ", texthl = "DiagnosticSignError" })
+    -- vim.fn.sign_define("DiagnosticSignWarn",
+    --   { text = " ", texthl = "DiagnosticSignWarn" })
+    -- vim.fn.sign_define("DiagnosticSignInfo",
+    --   { text = " ", texthl = "DiagnosticSignInfo" })
+    -- vim.fn.sign_define("DiagnosticSignHint",
+    --   { text = "", texthl = "DiagnosticSignHint" })
     -- NOTE: this is changed from v1.x, which used the old style of highlight groups
     -- in the form "LspDiagnosticsSignWarning"
 
@@ -62,12 +59,12 @@ return {
           expander_highlight = "NeoTreeExpander",
         },
         icon = {
-          folder_closed = "",
-          folder_open = "",
-          folder_empty = "ﰊ",
+          folder_closed = "▸",
+          folder_open = "▾",
+          folder_empty = "",
           -- The next two settings are only a fallback, if you use nvim-web-devicons and configure default icons there
           -- then these will never be used.
-          default = "*",
+          default = "",
           highlight = "NeoTreeFileIcon"
         },
         modified = {
@@ -82,14 +79,14 @@ return {
         git_status = {
           symbols = {
             -- Change type
-            added     = "",  -- or "✚", but this is redundant info if you use git_status_colors on the name
-            modified  = "",  -- or "", but this is redundant info if you use git_status_colors on the name
-            deleted   = "✖", -- this can only be used in the git_status source
-            renamed   = "", -- this can only be used in the git_status source
+            added     = "", -- or "✚", but this is redundant info if you use git_status_colors on the name
+            modified  = "", -- or "", but this is redundant info if you use git_status_colors on the name
+            deleted   = "", -- this can only be used in the git_status source
+            renamed   = "", -- this can only be used in the git_status source
             -- Status type
             untracked = "",
-            ignored   = "",
-            unstaged  = "",
+            ignored   = "",
+            unstaged  = "",
             staged    = "",
             conflict  = "",
           }
